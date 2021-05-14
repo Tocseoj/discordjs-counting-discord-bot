ab99f538696e9a6af6502f7c68b7a33e82c37788
Joe Scot Marshall <joe.scot.marshall@gmail.com>
2021-05-14 13:53:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const { Client } = require('discord.js');
+const client = new Client({ ws: { intents: ['GUILDS', 'GUILD_MESSAGES'] } });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+function validCount(messages) {
+  console.log("Messages", messages)
+  let userCache = [];
+  let prevNumber = 0;
+  for (let i = 0; i < messages.length; i++) {
+    const m = messages[i];
+    
+    if (userCache.includes(m.user)) {
+      console.log("Failed user validation")
+      return false;
+    }
+    userCache.push(m.user)
+
+    if (m.content.match(/[0-9]+/g) === null) {
+      console.log("Failed regex validation")
+      return false;
+    }
+    
+    if (prevNumber > 0 && (+m.content) !== prevNumber - 1) {
+      console.log("Failed number validation")
+      return false;
+    }
+    prevNumber = (+m.content);
+  }
+  return true;
+}
+
+client.on('message', async (msg) => {
+  let collection = await msg.channel.messages.fetch({limit: 3})
+  let messages = collection.map((m) => ({ user: m.author.id, content: m.content }))
+  if (validCount(messages)) {
+    // Success
+    // msg.react('✅'); 
+    return;
+  }
+  // Fail
+  msg.react('🚫');
+});
+
+client.login(process.env.COUNTING_BOT);
