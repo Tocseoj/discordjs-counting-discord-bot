b6873195be530e370ebea4aa37c3757948028966
Joe Scot Marshall <joe.scot.marshall@gmail.com>
2021-05-27 13:40:01 -0400

    save

--- index.js
+++ index.js
@@ -15,10 +15,21 @@ const FOUL_COLUMNS = [
   'fouls_badnumber',
   'fouls_badcount',
 ]
+const WEEK = 604800
+const START = 1612656000 // Sunday, February 7, 2021 12:00:00 AM
+const DB_PATH = '/home/ec2-user/db/counting.db'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+let lastCheckedWeek = -1
+
+function getWeekNumber(timestamp = null) {
+  if (timestamp === null) {
+    const now = new Date()  
+    const utcMilllisecondsSinceEpoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000)  
+    const utcSecondsSinceEpoch = Math.round(utcMilllisecondsSinceEpoch / 1000)
+    timestamp = utcSecondsSinceEpoch
+  }
+  return Math.floor((timestamp - START) / WEEK) + 1
+}
 
 // Just returns what I need
 function reduceMessage(message) {
@@ -61,6 +72,39 @@ function validCount(messages, skipUserValidation=false) {
   return foul;
 }
 
+function addError(message, foul) {
+  let updatedCount = 0
+  const db = new Database(DB_PATH);
+  const user = db.prepare(`SELECT * FROM counters WHERE snowflake  = ?`).get(message.author.id);
+  if (user) {
+    updatedCount = user[FOUL_COLUMNS[foul]] + 1
+  } else {
+    db.prepare(`INSERT INTO counters(snowflake,username,discriminator,avatar) VALUES(?, ?, ?, ?)`).run(message.author.id, message.author.username, message.author.discriminator, message.author.avatar);
+    updatedCount = 1
+  }
+  db.prepare(`UPDATE counters SET ${FOUL_COLUMNS[foul]} = ? WHERE snowflake = ?`).run(updatedCount, message.author.id);
+  db.close();
+}
+
+async function weekCounts(message) {
+  const db = new Database(DB_PATH);
+  const recent = db.prepare(`SELECT MAX(week) as last_week FROM weekcounts`).get();
+  const current_week = getWeekNumber(message.createdTimestamp)
+  if (recent.last_week != current_week) {
+    lastCheckedWeek = current_week
+    console.log("First message of a new week.", current_week)
+    db.prepare(`INSERT INTO weekcounts(snowflake,week,total_count,message_id) VALUES(?, ?, ?, ?)`).run("0", current_week, 0, message.id);
+    // Should probably close before calling an ayncronous method
+    // let collection = await message.channel.messages.fetch({ limit: 2, before: message.id })
+  }
+  db.close();
+}
+
+// DiscordJS Coe
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('message', async (msg) => {
   if (msg.author.bot) return
   if (msg.channel.id !== process.env.COUNTING_CHANNEL) return
@@ -72,23 +116,35 @@ client.on('message', async (msg) => {
   if (foul === FOUL_TYPES['ALL_GOOD']) {
     // Success
     // msg.react('✅'); 
-    return
-  }
-  // Fail
-  // msg.react('🚫');
-  msg.delete()
-
-  const db = new Database('/home/ec2-user/db/counting.db');
-  let updatedCount = 0
-  const user = db.prepare(`SELECT * FROM counters WHERE snowflake  = ?`).get(msg.author.id);
-  if (user) {
-    updatedCount = user[FOUL_COLUMNS[foul]] + 1
   } else {
-    db.prepare(`INSERT INTO counters(snowflake,username,discriminator,avatar) VALUES(?, ?, ?, ?)`).run(msg.author.id, msg.author.username, msg.author.discriminator, msg.author.avatar);
-    updatedCount = 1
+    // Fail
+    // msg.react('🚫');
+    msg.delete()
+  
+    // Track mistakes
+    try {
+      addError(msg, foul)
+    } catch (e) {
+      console.log("addError failed", e)
+      await setTimeout(function () {
+        addError(msg, foul)
+      }, 1000);
+    }
   }
-  db.prepare(`UPDATE counters SET ${FOUL_COLUMNS[foul]} = ? WHERE snowflake = ?`).run(updatedCount, msg.author.id);
-  db.close();
+
+  // This if statement can help reduce db accesses, 
+  // but isn't perfect since resets when the process restarts
+  if (getWeekNumber(msg.createdTimestamp) > lastCheckedWeek) {
+    // Get week counts once a week
+    try {
+      weekCounts(msg)
+    } catch (e) {
+      console.log("weekCounts failed", e)
+      await setTimeout(function () {
+        weekCounts(msg)
+      }, 1000);
+    }
+  }
 });
 
 client.login(process.env.COUNTING_BOT);
